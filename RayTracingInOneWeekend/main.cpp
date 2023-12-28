01dadc67a97835954ca81d4c2f4a658a0ae23fdd
Marcelo Shen <caogangshen@gmail.com>
2023-12-27 23:33:26 -0800

    setup png generation

--- RayTracingInOneWeekend/main.cpp
+++ RayTracingInOneWeekend/main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define _CRT_SECURE_NO_WARNINGS 
+#include "stb_image_write.h"
+
+// Image
+const int image_width = 256;
+const int image_height = 256;
+char data[image_height * image_width * 3];
 
 int main() {
 
-    // Image
 
-    int image_width = 256;
-    int image_height = 256;
 
     // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    //std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; ++j) {
         for (int i = 0; i < image_width; ++i) {
+            int dataInd = j * image_width * 3 + i * 3;
+
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
             auto b = 0;
@@ -21,7 +28,13 @@ int main() {
             int ig = static_cast<int>(255.999 * g);
             int ib = static_cast<int>(255.999 * b);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            data[dataInd + 0] = ir;
+            data[dataInd + 1] = ig;
+            data[dataInd + 2] = ib;
+
+            //std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    stbi_write_png("result.png", image_width, image_height, 3, data, image_width * 3);
 }
